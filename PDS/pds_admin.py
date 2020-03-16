f4f37222f7373e1f154597b76878edb48f25d8b9
Nikos Fotiou <nikosft@gmail.com>
2020-03-16 22:02:54 +0200

    Administrative interface

--- PDS/pds_admin.py
+++ PDS/pds_admin.py
@@ -13,15 +13,10 @@ conf = {}
 wallet_handle = ""
 pool_handle = ""
 
-class PDS:
-    @staticmethod
-    def generate_token(private_key, audience=None):
-        token = jwt.encode({'aud': 'sofie-iot.eu'},private_key, algorithm='RS256')
-        return 200, {'code':200,'message':token.decode('utf-8')}
-class PDSHandler(BaseHTTPRequestHandler):
+class PDSAdminHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         path = self.path
-        if path == "/gettoken":
+        if path == "/add":
             code = 403
             output = {'code':403, 'message':'Invalide or missing input parameters'}
             form = cgi.FieldStorage(
@@ -30,21 +25,33 @@ class PDSHandler(BaseHTTPRequestHandler):
                 environ={'REQUEST_METHOD':'POST',
                         'CONTENT_TYPE':self.headers['Content-Type'],
                         })
-            type  = form.getvalue("grant-type")
-            grant = form.getvalue("grant")
-            challenge = form.getvalue("challenge")
-            proof = form.getvalue("proof")
-            target = form.getvalue("target")
-            if (type == "DID"):
+            ndid  = form.getfirst("did","")
+            verkey = form.getfirst("verkey","")
+            metadata = form.getfirst("metadata","")
+            if (ndid !=""):
                 loop = asyncio.new_event_loop()
                 asyncio.set_event_loop(loop)
                 code, output = loop.run_until_complete(
-                    Indy.verify_did(grant, challenge, proof, wallet_handle,pool_handle, True))
-                loop.close()
-                if (code == 200):
-                    with open(conf['as_private_key'], mode='rb') as file: 
-                        as_private_key = file.read()
-                    code, output = PDS.generate_token(as_private_key, target)
+                    Indy.add_did_to_wallet(wallet_handle, ndid, verkey, metadata))
+            self.send_response(code)
+            self.send_header('Content-type','application/json')
+            self.end_headers()
+            self.wfile.write(json.dumps(output).encode())
+        if path == "/get":
+            code = 403
+            output = {'code':403, 'message':'Invalide or missing input parameters'}
+            form = cgi.FieldStorage(
+                fp = self.rfile, 
+                headers=self.headers,
+                environ={'REQUEST_METHOD':'POST',
+                        'CONTENT_TYPE':self.headers['Content-Type'],
+                        })
+            ndid  = form.getfirst("did","")
+            if (ndid !=""):
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                code, output = loop.run_until_complete(
+                    Indy.get_did_metadata(wallet_handle, ndid))
             self.send_response(code)
             self.send_header('Content-type','application/json')
             self.end_headers()
@@ -59,14 +66,16 @@ def main():
         sys.exit()
     with open(sys.argv[1]) as f:
         conf = json.load(f)
-    httpd     = HTTPServer(('', conf["port"]), PDSHandler)
+    httpadmin = HTTPServer(('', conf["adminport"]), PDSAdminHandler)
     loop = asyncio.get_event_loop()
     wallet_handle = loop.run_until_complete(wallet.open_wallet(json.dumps(conf['wallet_config']), json.dumps(conf['wallet_credentials'])))
+    
     try:
-        httpd.serve_forever()
+        httpadmin.serve_forever()
     except KeyboardInterrupt:
         pass
-    httpd.server_close()
+    httpadmin.server_close()
+  
     loop.run_until_complete(wallet.close_wallet(wallet_handle))
 
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -8,15 +8,19 @@ import requests
 import json
 import asyncio
 import base64
+import datetime
+import time
 
 @pytest.fixture(autouse=True)
 def server():
     import subprocess
     import time
     p1 = subprocess.Popen(['python3', 'PDS/pds.py', 'tests/conf/test.conf'])
+    p2 = subprocess.Popen(['python3', 'PDS/pds_admin.py', 'tests/conf/test.conf'])
     time.sleep(1) #Otherwise the server is not ready when tests start
     yield
     p1.kill()
+    p2.kill()
 
 
 
@@ -38,6 +42,27 @@ async def test_valid_did():
     signature64 = base64.b64encode(signature)
     payload = {'grant-type':'DID', 'grant':user['did'], 'challenge': challenge, 'proof':signature64, 'target':'sofie-iot.eu'}
     response  = requests.post("http://localhost:9001/gettoken", data = payload).text
+    response =json.loads(response)
+    assert(response['code'] == 200)
+    await wallet.close_wallet(wallet_handle)
+
+@pytest.mark.asyncio
+async def test_add_did():
+    user = {
+        'wallet_config': json.dumps({'id': 'user_wallet',"storage_config":{"path":"tests/indy_wallets"}}),
+        'wallet_credentials': json.dumps({'key': 'user_wallet_key'}),
+        'did' : '4qk3Ab43ufPQVif4GAzLUW'
+    }
+    wallet_handle = await wallet.open_wallet(user['wallet_config'], user['wallet_credentials'])
+    verkey = await did.key_for_local_did(wallet_handle, user['did'])
+    nbf = time.mktime(datetime.datetime(2020, 4, 1, 00, 00).timetuple())
+    exp = time.mktime(datetime.datetime(2020, 4, 1, 23, 59).timetuple()) 
+    payload = {'did':user['did'], 'verkey': verkey, 'metadata':json.dumps({'aud': 'sofie-iot.eu','nbf':nbf, 'exp': exp})}
+    response  = requests.post("http://localhost:9002/add", data = payload).text
+    response =json.loads(response)
+    assert(response['code'] == 200)
+    payload = {'did':user['did']}
+    response  = requests.post("http://localhost:9002/get", data = payload).text
     response =json.loads(response)
     assert(response['code'] == 200)
     await wallet.close_wallet(wallet_handle)
