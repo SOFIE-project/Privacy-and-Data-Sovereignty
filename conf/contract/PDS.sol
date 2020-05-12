bfc526e99f66e7463031ce2c7a103d7f717246f8
Nikos Fotiou <nikosft@gmail.com>
2020-05-12 19:28:51 +0300

    modifications to sample contract

--- conf/contract/PDS.sol
+++ conf/contract/PDS.sol
@@ -1,25 +1,15 @@
-pragma solidity ^0.4.0;
-
-contract PDS
-{
-    
-    struct token_entry{
+pragma solidity ^0.5.0;
+import { Ownable } from "@openzeppelin/contracts/ownership/Ownable.sol";
+contract PDS is Ownable {
+    event token_added (uint index, bytes DID);
+    struct token_entry {
+        bytes metadata;
         bytes DID;
         bytes enc_token;
     }
-    
     token_entry[] tokens;
-    
-    event token_added (uint index, bytes DID);
-    
-    function new_token (bytes DID, bytes enc_token) public 
-    {
-        tokens.push(token_entry(DID, enc_token));
+    function new_token(bytes memory metadata, bytes memory DID, bytes memory enc_token) public onlyOwner {
+        tokens.push(token_entry(metadata, DID, enc_token));
         emit token_added(tokens.length-1, DID);
-    }
-    
-    function get_token(uint index) public view returns (bytes DID, bytes enc_token)
-    {
-        return(tokens[index].DID, tokens[index].enc_token);
     }
 }
