c2e0984d6abccaf689a3cd65656eef2796761d0c
Nikos Fotiou <fotiou@aueb.gr>
2020-06-23 02:01:12 +0300

    Token encryption added

--- PDS/pds.py
+++ PDS/pds.py
@@ -10,36 +10,36 @@ import asyncio
 import base64
 import sys
 import jwt
+import nacl.encoding
+from nacl.public import SealedBox
 
 class PDS:
     def __init__(self, wallet_handle=None, pool_handle=None):
         self.wallet_handle = wallet_handle
         self.pool_handle = pool_handle
 
-    def generate_token(self, private_key, audience=None,  subject=None, expires=None, nbf = None, token_type=None ):
+    def generate_token(self, private_key, metadata = None, enc_key=None, token_type=None ):
         claims = {}
-        if audience:
-            claims['aud'] = audience
-        if subject:
-            claims['sub'] = subject
-        if expires:
-            claims['exp'] = expires
-        if nbf:
-            claims['nbf'] = nbf
+        metadata = json.loads(metadata)
+        if 'aud' in metadata:
+            claims['aud'] = metadata['aud']
+        if 'sub' in metadata:
+            claims['sub'] = metadata['sub']
+        if 'exp' in metadata:
+            claims['exp'] = metadata['exp']
+        if 'nbf' in metadata:
+            claims['nbf'] = metadata['nbf']
         token = jwt.encode(claims,private_key, algorithm='RS256')
-        if token_type == None:
-            return 200, {'code':200,'message':token.decode('utf-8')}
-        if token_type == "DID-encrypted":
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            code, output = loop.run_until_complete(
-                Indy.encrypt_for_did(subject, token.decode('utf-8'), self.wallet_handle, self.pool_handle, True)
-            )
-            loop.close()
-            return code, output
+        if enc_key:
+            public_key = nacl.public.PublicKey(enc_key,nacl.encoding.HexEncoder)
+            sealed_box = SealedBox(public_key)
+            token = sealed_box.encrypt(token)
+            token = base64.urlsafe_b64encode(token)
+        return 200, {'code':200,'message':token.decode('utf-8')}
+
     
-    def log_token(self, logged_DID, logged_token, web3_provider, eth_account, PDSContract_instance):
-        tx_hash = PDSContract_instance.functions.new_token(web3_provider.toBytes(text=logged_DID), web3_provider.toBytes(text=logged_token)).transact({'from': eth_account})
+    def log_token(self, metadata, logged_token, web3_provider, eth_account, PDSContract_instance):
+        tx_hash = PDSContract_instance.functions.new_token(web3_provider.toBytes(text=metadata), web3_provider.toBytes(text=logged_token)).transact({'from': eth_account})
         web3_provider.eth.waitForTransactionReceipt(tx_hash)
         return 200, {'code':200,'message':'token logged'}
 
@@ -54,11 +54,11 @@ class PDSHandler():
             self.pds = PDS(self.wallet_handle, self.pool_handle)
             self.web3_provider = Web3(Web3.HTTPProvider(self.conf['web3provider']))
             self.eth_account = self.web3_provider.eth.accounts[0]
-            with open('conf/contract/PDS.abi', 'r') as myfile:
+            with open('conf/contract/build/PDS.abi', 'r') as myfile:
                 self.abi = myfile.read()
             self.PDSContract_instance = self.web3_provider.eth.contract(abi=self.abi, address=Web3.toChecksumAddress(self.conf['pds_sc_address']))
         except:
-            print("Couldn't connect to Ethereum blockchain")
+            print("Couldn't connect to Ethereum blockchain:" + self.conf['web3provider'])
             pass
 
     def wsgi_app(self, environ, start_response):
@@ -71,13 +71,10 @@ class PDSHandler():
         grant       = form.get("grant", None)
         challenge   = form.get("challenge", None)
         proof       = form.get("proof", None)
-        target      = form.get("target", None)
         token_type  = form.get("token-type", None)
-        subject     = form.get("subject", None)
         log_token   = form.get("log-token", None)
-        expires     = form.get("expires", None)
-        nbf         = form.get("nbf", None)
-        
+        enc_key   = form.get("enc-key", None)
+
         if (grant_type == "DID"):
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
@@ -97,17 +94,17 @@ class PDSHandler():
                 asyncio.set_event_loop(loop)
                 _, output = loop.run_until_complete(
                     Indy.get_did_metadata(self.wallet_handle, grant))
-                target  = json.loads(output['message'])['aud']
-                expires = json.loads(output['message'])['exp']
-                nbf     = json.loads(output['message'])['nbf']
+                metadata  = output['message']
         if (grant_type == "auth_code"):
+            metadata  = form.get("metadata", None)
             code = 200
         if (code == 200):
             with open(self.conf['as_private_key'], mode='rb') as file: 
                 as_private_key = file.read()
-            code, output = self.pds.generate_token(as_private_key, target, subject, expires, nbf, token_type)
-        if (log_token == 'True' and code == 200):
-            self.pds.log_token(subject,output['message'], self.web3_provider,self.eth_account, self.PDSContract_instance)
+            code, output = self.pds.generate_token(as_private_key, metadata, enc_key)
+        if (log_token and code == 200):
+            self.pds.log_token(log_token, output['message'], self.web3_provider,self.eth_account, self.PDSContract_instance)
+            print("token logged")
         response = Response(json.dumps(output).encode(), status=code, mimetype='application/json')
         return response(environ, start_response)
     
