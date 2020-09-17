d39c5dc875142534cd7aa292d958224d832674c1
Nikos Fotiou <fotiou@aueb.gr>
2020-09-17 14:21:30 +0300

    bugs in tests fixed

--- tests/test_api_with_indy_did.py
+++ tests/test_api_with_indy_did.py
@@ -30,20 +30,18 @@ async def test_valid_did():
         'did' : '4qk3Ab43ufPQVif4GAzLUW'
     }
     payload = {'grant-type':'DID', 'grant':user['did'], 'target':'smartlocker1'}
-    response  = requests.post("http://localhost:9001/gettoken", data = payload).text
-    response =json.loads(response)
-    print(response)
-    assert(response['code'] == 401)
-    challenge = response['challenge']
+    response  = requests.post("http://localhost:9001/gettoken", data = payload)
+    assert(response.status_code == 401)
+    challenge = response.text
     wallet_handle = await wallet.open_wallet(user['wallet_config'], user['wallet_credentials'])
     verkey = await did.key_for_local_did(wallet_handle, user['did'])
     signature = await crypto.crypto_sign(wallet_handle, verkey, challenge.encode())
     signature64 = base64.b64encode(signature)
     exp = time.mktime(datetime.datetime(2020, 4, 1, 23, 59).timetuple())
     payload = {'grant-type':'DID', 'grant':user['did'], 'challenge': challenge, 'proof':signature64}
-    response  = requests.post("http://localhost:9001/gettoken", data = payload).text
-    response =json.loads(response)
-    assert(response['code'] == 200)
+    response  = requests.post("http://localhost:9001/gettoken", data = payload)
+    token = response.text
+    assert(response.status_code == 200)
     await wallet.close_wallet(wallet_handle)
 '''
 @pytest.mark.asyncio
